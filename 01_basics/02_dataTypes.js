d14221df525a1cf33134d53f72e946e29d826858
protonshub-arihant <103586259+protonshub-arihant@users.noreply.github.com>
2023-11-21 04:25:22 +0000

    data type learning

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all js as newer version
+
+console.log("Arihant")
+
+//datatypes
+
+//number;
+//bignit;
+//boolean;
+//string;
+//null => standalone value
+//undefined => not initialized right now ex = let state;
+//symbol= > uniq
+
+
+//object
+console.log(typeof "Arihant");
+console.log(typeof null) // output object
+console.log(typeof undefined) //output undefined
